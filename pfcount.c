7f7190125d087e5dbdf991e83c46f654db800420
mephistolist <49227141+mephistolist@users.noreply.github.com>
2025-01-14 06:44:45 +0000

    Update pfcount.c

--- pfcount.c
+++ pfcount.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Function to execute a shell command and return the output line count
-unsigned long count_lines_from_command(const char *command) {
+// Function to execute a shell command and retrieve its output as an unsigned long
+unsigned long execute_command_and_get_result(const char *command) {
     FILE *fp = popen(command, "r");
     if (!fp) {
         perror("popen");
         return 0;
     }
 
-    unsigned long line_count = 0;
-    char buffer[256];
-
-    while (fgets(buffer, sizeof(buffer), fp)) {
-        line_count++;
+    unsigned long result = 0;
+    if (fscanf(fp, "%lu", &result) != 1) {
+        fprintf(stderr, "Failed to parse the output of the command.\n");
     }
 
     pclose(fp);
-    return line_count;
+    return result;
 }
 
 int pfcount_main(void) {
-    const char *command = "for i in $(pfctl -sT); do pfctl -t $i -T show; done";
-    unsigned long total_ips = count_lines_from_command(command);
+    const char *command = "find /var/pfpb/tables/ -type f -exec wc -l {} \\; | awk '{total += $1} EN;
+    unsigned long total_lines = execute_command_and_get_result(command);
 
-    printf("Total IP Ranges Blocked: %lu\n", total_ips);
+    printf("Total IP Ranges Blocked: %lu\n", total_lines);
     return EXIT_SUCCESS;
 }
