0b7f57515a22d8d9c38958a7e6a01be7253c12b5
mephistolist <49227141+mephistolist@users.noreply.github.com>
2025-01-14 06:42:45 +0000

    Update main.c

--- main.c
+++ main.c
@@ -26,9 +26,9 @@ void set_table_entries(const int *value);
 void print_usage(const char *program_name) {
     printf("Usage: %s <command>\n", program_name);
     printf("Commands:\n");
-    printf(" pfpb start -  Start load the PF tables containing ip ranges from iblocklist to block.\n");
+    printf(" pfpb start -  Start load the PF tables containing ip ranges from iblocklist to block.\n;
     printf(" pfpb stop  -  Stop all PF tables with ips ranges from being blocked.\n");
-    printf(" pfpb update - Update any new blocklists so if any new ip ranges exist, they can be applied.\n");
+    printf(" pfpb update - Update any new blocklists so if any new ip ranges exist, they can be appl;
 }
 
 // Function to save the original table-entries value to a file
@@ -96,9 +96,7 @@ void run_quiet(const char *command) {
     int ret = system(command);
     if (ret == -1) {
         perror("system");
-    } /*else if (WIFEXITED(ret) && WEXITSTATUS(ret) != 0) {
-        fprintf(stderr, "Command failed with exit code %d: %s\n", WEXITSTATUS(ret), command);
-    }*/
+    }
 }
 
 int main(int argc, char *argv[]) {
